b238baf4bca4dde41a0ca44090f210413ba27b8e
Christian <cjalcala@usc.edu>
2026-04-30 19:21:40 -0700

    fix: add viewport meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "@/styles/globals.css";
 
 export const metadata: Metadata = {
@@ -8,6 +8,11 @@ export const metadata: Metadata = {
   icons: {
     icon: "/images/logo/rosecityLogo.jpeg",
   },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({
